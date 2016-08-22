5a59e91ac77bedc8cc1a5a9969153d93685a88d4
colinbfmarshall <colinbfmarshall@gmail.com>
2016-08-23 00:54:49 +0100

    added a photo url to the player

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,2 +1,11 @@
 module PlayersHelper
+
+def goalie(player)
+  if player.outfield == true
+    "Outfield"
+  else
+    "Goalie"
+  end
+end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822225613) do
+ActiveRecord::Schema.define(version: 20160822232611) do
 
   create_table "players", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160822225613) do
     t.string   "nationality"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "photo"
   end
 
 end
